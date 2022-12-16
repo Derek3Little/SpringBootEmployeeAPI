332ecba4190881a6d3efcda90b75b05c4bf0c4b2
Derek E Little <derek3little@gmail.com>
2022-12-16 12:18:37 -0500

    Initial commit, starting SpringBootEmployeeAPI with Spring Boot Initializr.

---
+++ src/main/java/com/example/SpringBootEmployeeAPI/SpringBootEmployeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootEmployeeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootEmployeeAPI/SpringBootEmployeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootEmployeeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmployeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0049b02705e7c5cefecffb22cd4953b6247e58e7
Derek E Little <derek3little@gmail.com>
2022-12-16 15:17:15 -0500

    Creating Employee and EmployeeController classes.

---
+++ src/main/java/com/example/SpringBootEmployeeAPI/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.example.SpringBootEmployeeAPI.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+public class EmployeeController {
+    // @8:04
+}
---
+++ src/main/java/com/example/SpringBootEmployeeAPI/entity/Employee.java
@@ -0,0 +1,5 @@
+package com.example.SpringBootEmployeeAPI.entity;
+
+public class Employee {
+
+}

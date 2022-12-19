1311741d1fc9c29f945ecf2a79e5faa4d046c3a2
Derek E Little <derek3little@gmail.com>
2022-12-19 10:58:38 -0500

    Adding fields/constructor to Employee and associated employeeList function to EmployeeController

--- src/main/java/com/example/SpringBootEmployeeAPI/controller/EmployeeController.java
+++ src/main/java/com/example/SpringBootEmployeeAPI/controller/EmployeeController.java
@@ -1,10 +1,16 @@
 package com.example.SpringBootEmployeeAPI.controller;
 
+import com.example.SpringBootEmployeeAPI.entity.Employee;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class EmployeeController {
-    // @8:04
+    public List<Employee> employeeList;
+
+    public List<Employee> findAllEmployees() {
+        return employeeList;
+    }
 }
--- src/main/java/com/example/SpringBootEmployeeAPI/entity/Employee.java
+++ src/main/java/com/example/SpringBootEmployeeAPI/entity/Employee.java
@@ -1,5 +1,13 @@
 package com.example.SpringBootEmployeeAPI.entity;
 
 public class Employee {
+    int employeeId;
+    String employeeName;
+    String employeeCity;
 
+    public Employee(int employeeId, String employeeName, String employeeCity) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.employeeCity = employeeCity;
+    }
 }

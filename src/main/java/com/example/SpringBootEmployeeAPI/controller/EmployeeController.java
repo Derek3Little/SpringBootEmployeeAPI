14b7ac6c593d505f8a4710c5262d0c89fa7d1099
Derek E Little <derek3little@gmail.com>
2022-12-20 10:04:23 -0500

    Adding findAnEmployee function.

--- src/main/java/com/example/SpringBootEmployeeAPI/controller/EmployeeController.java
+++ src/main/java/com/example/SpringBootEmployeeAPI/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.SpringBootEmployeeAPI.controller;
 import com.example.SpringBootEmployeeAPI.entity.Employee;
 import com.example.SpringBootEmployeeAPI.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,10 @@ public class EmployeeController {
     @RequestMapping("/employees")
     public List<Employee> findAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @RequestMapping("/employees/{id}") // passing {id} into findAnEmployee() via @PathVariable!
+    public Employee findAnEmployee(@PathVariable int id) {
+        return employeeService.getAnEmployee(id);
     }
 }

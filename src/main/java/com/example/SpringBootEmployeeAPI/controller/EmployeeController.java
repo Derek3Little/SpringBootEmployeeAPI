a6ceb1a6afb6d8e4d6b701cfff5c293982d6c5d2
Derek E Little <derek3little@gmail.com>
2022-12-20 10:31:19 -0500

    Adding createEmployee requestmapping for POST method.

--- src/main/java/com/example/SpringBootEmployeeAPI/controller/EmployeeController.java
+++ src/main/java/com/example/SpringBootEmployeeAPI/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.example.SpringBootEmployeeAPI.controller;
 import com.example.SpringBootEmployeeAPI.entity.Employee;
 import com.example.SpringBootEmployeeAPI.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,5 +22,10 @@ public class EmployeeController {
     @RequestMapping("/employees/{id}") // passing {id} into findAnEmployee() via @PathVariable!
     public Employee findAnEmployee(@PathVariable int id) {
         return employeeService.getAnEmployee(id);
+    }
+
+    @RequestMapping(value = "/employees", method = RequestMethod.POST)
+    public void createEmployee(@RequestBody Employee employee) {
+        employeeService.createEmployee(employee);
     }
 }

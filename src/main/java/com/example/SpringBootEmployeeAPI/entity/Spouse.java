093a2d61af82fb43812307953ae97a58612c187e
Derek E Little <derek3little@gmail.com>
2022-12-28 12:35:56 -0500

    Creating Spouse entity and class.

---
+++ src/main/java/com/example/SpringBootEmployeeAPI/entity/Spouse.java
@@ -0,0 +1,54 @@
+package com.example.SpringBootEmployeeAPI.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Spouse")
+public class Spouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String mobileNumber;
+    private int age;
+
+    public Spouse(int id, String name, String mobileNumber, int age) {
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
